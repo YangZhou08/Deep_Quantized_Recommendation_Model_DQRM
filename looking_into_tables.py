93ac1b787c55b22190b31c32c282c495123d0373
YangZhou08 <yangzhou25672@utexas.edu>
2022-07-26 15:14:46 -0700

    Create looking_into_tables.py

---
+++ looking_into_tables.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+path = "/rscratch/data/dlrm_criteo/" 
+table_num = 0 
+file_name_one = "table" + str(table_num) + "epoch" + "0.txt" 
+file_name_two = "table" + str(table_num) + "epoch" + "1.txt" 
+
+list_one = [] 
+file_path = path + file_name_one 
+file = open(file_path, "r") 
+lines = file.readlines() 
+for line in lines: 
+    line_seg = line.split(", ") 
+    for word_with_value in line_seg: 
+        list_one.append(float(word_with_value)) 
+print(len(list_one)) 
+
+plt.hist(list_one) 
+plt.show() 
