4fb346c8e2b2fd3ea19daeed031c1dcb037ea725
YangZhou08 <yangzhou25672@utexas.edu>
2022-07-27 01:09:50 -0700

    change

--- looking_into_tables.py
+++ looking_into_tables.py
@@ -21,28 +21,39 @@ file_names = [file_name_one, file_name_two]
 fig, axes = plt.subplots(1, 2) 
 
 list_one = [] 
-for i, file_name in enumerate(file_names): 
-    file_path = path + file_name_two 
-    file = open(file_path, "r") 
-    lines = file.readlines() 
-    for line in lines: 
-        line_seg = line.split(", ") 
-        for word_with_value in line_seg: 
-            list_one.append(float(word_with_value)) 
-    print(len(list_one)) 
+for table_num in range(2, 26): 
+    file_name_one = "table" + str(table_num) + "epoch" + "0.txt" 
+    file_name_two = "table" + str(table_num) + "epoch" + "1.txt" 
 
-    min = np.min(list_one) 
-    max = np.max(list_one) 
+    file_names = [file_name_one, file_name_two] 
+    
+    for i, file_name in enumerate(file_names): 
+        file_path = path + file_name_two 
+        file = open(file_path, "r") 
+        lines = file.readlines() 
+        for line in lines: 
+            line_seg = line.split(", ") 
+            for word_with_value in line_seg: 
+                list_one.append(float(word_with_value)) 
+        print(len(list_one)) 
 
-    axes[i].hist(list_one, bins = 100) 
-    axes[i].axvline(min, color = 'red', lw = 2) 
-    axes[i].axvline(max, color = 'red', lw = 2) 
-    '''
-    axes[i].set_title("Table {}\nmin {:.2f} max {:.2f}".format(table_num, min, max)) 
-    ''' 
-    axes[i].set_title("Table {}\nmin {:.4f} max {:.4f}".format(table_num, min, max)) 
+        min = np.min(list_one) 
+        max = np.max(list_one) 
 
-    list_one = [] 
+        axes[i].hist(list_one, bins = 100) 
+        axes[i].axvline(min, color = 'red', lw = 2) 
+        axes[i].axvline(max, color = 'red', lw = 2) 
+        '''
+        axes[i].set_title("Table {}\nmin {:.2f} max {:.2f}".format(table_num, min, max)) 
+        ''' 
+        axes[i].set_title("Table {}\nmin {:.4f} max {:.4f}".format(table_num, min, max)) 
+
+        list_one = [] 
+    axes[0].clear() 
+    axes[1].clear() 
+
+    print("printing table {} distribution".format(table_num)) 
+    plt.savefig("hist" + str(table_num) + ".png") 
 
 '''
 plt.hist(list_one, bins = 100) 
@@ -52,5 +63,3 @@ plt.vlines(min, 0, 1100, color = 'red', linestyles = 'dashed')
 plt.vlines(max, 0, 1100, color = 'red', linestyles = 'dashed') 
 plt.title("Table {} min: {} max: {}".format(table_num, min, max)) 
 ''' 
-
-plt.savefig("hist.png") 
