12255b33dfddf67b9ef7eb7c7c7f1b319eb9f88b
YangZhou08 <yangzhou25672@utexas.edu>
2022-07-28 11:36:53 -0700

    changee

--- dlrm_s_pytorch_dp_only.py
+++ dlrm_s_pytorch_dp_only.py
@@ -779,7 +779,7 @@ def run():
     '''
     os.environ['MASTER_ADDR'] = '169.229.49.62' 
     ''' 
-    os.environ['MASTER_ADDR'] = '169.229.49.63' 
+    os.environ['MASTER_ADDR'] = '169.229.49.61' 
     '''
     os.environ['MASTER_PORT'] = '29500' 
     ''' 
