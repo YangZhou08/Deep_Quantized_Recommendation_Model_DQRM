d15d89bd5f0373549e87cd02e8f5442b5f9ddecf
YangZhou08 <yangzhou25672@utexas.edu>
2022-08-05 17:46:54 -0700

    changes

--- dlrm_s_pytorch_dp_only.py
+++ dlrm_s_pytorch_dp_only.py
@@ -691,13 +691,15 @@ class DLRM_Net(nn.Module):
             return z 
         else: 
             x, act_scaling_factor = self.quant_input(dense_x) 
+            if act_scaling_factor is None: 
+                print("tuple is x") 
             '''
             x = self.apply_mlp(x, self.bot_l, prev_act_scaling_factor = act_scaling_factor) 
             ly = self.apply_emb(lS_o, lS_i, self.emb_l, self.v_W_l, test_mode = test_mode) 
             z, feature_scaling_factor = self.interact_features(x, ly) 
             p = self.apply_mlp(z, self.top_l, prev_act_scaling_factor = feature_scaling_factor) 
             ''' 
-            x = self.apply_mlp(x, self.bot_l) 
+            x = self.apply_mlp(x[0], self.bot_l) 
             ly = self.apply_emb(lS_o, lS_i, self.emb_l, self.v_W_l, test_mode = test_mode) 
             z = self.interact_features(x, ly) 
             p = self.apply_mlp(z, self.top_l) 
