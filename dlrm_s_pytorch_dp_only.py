3947af93f44ce04e5611846d9ff5b3e3b9504295
YangZhou08 <yangzhou25672@utexas.edu>
2022-08-09 17:38:57 -0700

    changesss

--- dlrm_s_pytorch_dp_only.py
+++ dlrm_s_pytorch_dp_only.py
@@ -680,7 +680,7 @@ class DLRM_Net(nn.Module):
                 Z_integers = torch.bmm(T_integers, torch.transpose(T_integers, 1, 2)) 
 
                 Z = Z_integers * (self.feature_scaling_factor ** 2) 
-
+                '''
                 with torch.no_grad(): 
                     print("max bound") 
                     print(self.feature_xmax) 
@@ -690,7 +690,7 @@ class DLRM_Net(nn.Module):
                     print(Z[0 : 10]) 
                     print("T dot production with T") 
                     print(torch.bmm(T, torch.transpose(T, 1, 2))[0 : 10]) 
-
+                ''' 
                 # incorporate features are copied 
                 _, ni, nj = Z.shape
                 # approach 1: tril_indices
