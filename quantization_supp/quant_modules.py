3907ef62ffec0c76bc2233cedbdb36cb847f60a1
YangZhou08 <yangzhou25672@utexas.edu>
2022-08-15 12:43:55 -0700

    change_oneone

--- quantization_supp/quant_modules.py
+++ quantization_supp/quant_modules.py
@@ -254,7 +254,7 @@ class QuantEmbeddingBagTwo(Module):
     def set_iteration_bound(self): 
         # fixed value 
         if self.iteration_nt == 1 and self.iteration_bound == 0: # change if do other things 
-            self.iteration_nt += 300 
+            self.iteration_bound += 300 
             print("bound increasing to {}".format(self.iteration_nt.item())) 
         '''
         elif self.iteration_bound == 100 and self.iteration_nt > 30000: 
