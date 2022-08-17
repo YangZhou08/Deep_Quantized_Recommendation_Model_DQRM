928970d7c7832dcc7e900b136df4ea481986632b
YangZhou08 <yangzhou25672@utexas.edu>
2022-08-17 10:34:22 -0700

    change_one_one_one

--- quantization_supp/quant_modules.py
+++ quantization_supp/quant_modules.py
@@ -287,12 +287,16 @@ class QuantEmbeddingBagTwo(Module):
                     raise Exception("for embedding weights, we only support symmetric quantization") 
             
                 # update period info 
+                '''
                 self.iteration_nt += 1 
                 self.now_iteration -= self.now_iteration 
                 self.set_iteration_bound() 
+                ''' 
             else: 
+                '''
                 self.iteration_nt += 1 
                 self.now_iteration += 1 
+                ''' 
             
         if per_sample_weights is not None: 
             print("Warning: Embedding Table Assumes per_sample_weights to be None but it is not") 
