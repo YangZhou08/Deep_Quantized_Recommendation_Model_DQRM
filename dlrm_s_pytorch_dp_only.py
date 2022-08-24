ea7cc70c283aa356337e8eb474f0d306d7bbda7c
YangZhou08 <yangzhou25672@utexas.edu>
2022-08-24 01:08:00 -0500

    changeoneoneoneoneeon

--- dlrm_s_pytorch_dp_only.py
+++ dlrm_s_pytorch_dp_only.py
@@ -1051,7 +1051,7 @@ def run():
     '''
     os.environ['MASTER_PORT'] = '29500' 
     ''' 
-    os.environ['MASTER_PORT'] = '29572' 
+    os.environ['MASTER_PORT'] = '29573' 
     os.environ['WORLD_SIZE'] = str(args.world_size) 
     mp.spawn(train, nprocs = args.gpus, args = (args,)) 
   
