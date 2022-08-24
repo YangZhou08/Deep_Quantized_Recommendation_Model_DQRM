8ab65c3c955993b76d1d45361279bfbf440a8430
YangZhou08 <yangzhou25672@utexas.edu>
2022-08-24 01:48:25 -0500

    change_one

--- sgd_quantized_gradients.py
+++ sgd_quantized_gradients.py
@@ -18,21 +18,12 @@ def quantized_gradients_update(model, arg, lr):
         world_size = float(arg.world_size) 
         for name, param in model.named_parameters(): 
             update = param.grad # finding the gradient of the data by layer 
-            if arg.world_size != 1: 
-                dist.all_reduce(update, op=dist.ReduceOp.SUM) 
-            '''
-            print(lr) 
-            ''' 
+            print(name) 
+            print(update.shape) 
             param.add_(update * (-lr[0])) 
-            '''
-            param.grad.data *= 0 
-            ''' 
 
 def clear_gradients(model): 
     with torch.no_grad(): 
         for name, param in model.named_parameters(): 
-            '''
-            print(name) 
-            ''' 
             if param.grad is not None: 
                 param.grad *= 0 
