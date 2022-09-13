12a76fd4bccb9d39ee244d940c43c3f27148e70c
YangZhou08 <yangzhou25672@utexas.edu>
2022-09-13 12:24:24 -0500

    changeo

--- looking_into_tables_and_quantize.py
+++ looking_into_tables_and_quantize.py
@@ -73,6 +73,7 @@ for file_name in file_names:
         plt.xlim(-0.3, 0.3) 
     else: 
         plt.hist(list_one, log = False, bins = 100) 
+        plt.xlim(-0.2, 0.2) 
     '''
     chicanes.append(np.sqrt(1/n_l[table_num])) 
     for ratio in thr_r: 
