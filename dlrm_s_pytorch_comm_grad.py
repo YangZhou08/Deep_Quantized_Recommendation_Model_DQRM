47f563d0980b5da19245945551bca2d9dcaab96f
YangZhou08 <yangzhou25672@utexas.edu>
2022-09-26 15:04:45 -0500

    changeo

--- dlrm_s_pytorch_comm_grad.py
+++ dlrm_s_pytorch_comm_grad.py
@@ -1151,7 +1151,7 @@ def run():
     '''
     os.environ['MASTER_ADDR'] = '169.229.49.62' 
     ''' 
-    os.environ['MASTER_ADDR'] = '172.17.0.1' 
+    os.environ['MASTER_ADDR'] = '169.254.3.1' 
     '''
     os.environ['MASTER_PORT'] = '29500' 
     ''' 
