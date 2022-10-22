725e843bc02930045428db3897a45e3134f317e3
YangZhou08 <yangzhou25672@utexas.edu>
2022-10-22 13:11:33 -0500

    changeo

--- discovering_rowise_hotness.py
+++ discovering_rowise_hotness.py
@@ -1537,8 +1537,8 @@ def train(gpu, args):
     for j, inputBatch in enumerate(train_loader): 
             X, lS_o, lS_i, T, W, CBPP = unpack_batch(inputBatch) 
             print("iteration {}, printing out offset and index".format(j)) 
-            print(lS_o[:, 0]) 
-            print(lS_i[:, 0]) 
+            print(lS_o) 
+            print(lS_i) 
 
 if __name__ == "__main__": 
     run() 
