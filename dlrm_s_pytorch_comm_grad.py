0d44ca73d82034138fe59b0644995047ffad5422
YangZhou08 <yangzhou25672@utexas.edu>
2022-11-01 11:47:21 -0500

    changeo

--- dlrm_s_pytorch_comm_grad.py
+++ dlrm_s_pytorch_comm_grad.py
@@ -1149,11 +1149,11 @@ def run():
     '''
     os.environ['MASTER_ADDR'] = '169.229.49.62' 
     ''' 
-    os.environ['MASTER_ADDR'] = '169.229.49.61' 
+    os.environ['MASTER_ADDR'] = '169.254.3.1' 
     '''
     os.environ['MASTER_PORT'] = '29500' 
     ''' 
-    os.environ['MASTER_PORT'] = '29515' 
+    os.environ['MASTER_PORT'] = '29511' 
     os.environ['WORLD_SIZE'] = str(args.world_size) 
     mp.spawn(train, nprocs = args.gpus, args = (args,)) 
   
