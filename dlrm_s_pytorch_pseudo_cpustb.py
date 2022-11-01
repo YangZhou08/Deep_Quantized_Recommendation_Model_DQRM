74df8c473a2e212eef3db54da02bd93178a85f7e
YangZhou08 <yangzhou25672@utexas.edu>
2022-11-01 13:48:08 -0500

    changeo

--- dlrm_s_pytorch_pseudo_cpustb.py
+++ dlrm_s_pytorch_pseudo_cpustb.py
@@ -1596,23 +1596,20 @@ def train(gpu, args):
                 grad_buffer_update_added_quantization(dlrm, args.number_of_cpu_node, emb_grad_quantized = False) 
                 ''' 
                 optimizer.step() 
+                '''
                 if j % args.number_of_cpu_node == 0: 
-                    '''
                     print("updating weights") 
-                    ''' 
-                    '''
                     weights_update(dlrm, lr_scheduler.get_lr()[-1]) 
-                    ''' 
-                    '''
                     if k >= 3: 
                         weights_update_added_quantization(dlrm, lr_scheduler.get_lr()[-1], args.number_of_gpus, emb_grad_quantized = False, update_embedding = False) 
                     else: 
                         weights_update_added_quantization(dlrm, lr_scheduler.get_lr()[-1], args.number_of_gpus, emb_grad_quantized = False) 
-                    ''' 
+                    # used previously 
                     weights_update_added_quantization(dlrm, lr_scheduler.get_lr()[-1], args.number_of_cpu_node, emb_grad_quantized = False, update_embedding = True) 
                     lr_scheduler.step() 
                     buffer_clean = True 
                     print("step {} weight updated".format(j)) 
+                ''' 
                 '''
                 dlrm.show_output_linear_layer_grad() 
                 ''' 
