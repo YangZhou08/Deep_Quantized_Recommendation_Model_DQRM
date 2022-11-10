a578d869404a3c659c37e97dd4cd4241b352e7fa
YangZhou08 <yangzhou25672@utexas.edu>
2022-11-10 12:17:30 -0600

    changeo

--- profiling_cpus.sh
+++ profiling_cpus.sh
@@ -1,4 +1,5 @@
 #PBS -l select=2:ncpus=160 -lplace=excl 
+conda init bash 
 conda activate aikit-pt 
 export LD_LIBRARY_PATH=~/anaconda3/lib 
 mpirun -n 2 -l python /homes/yangzhou08/Training_DLRM_fast/example_multiple_cpu_dp_two.py 
