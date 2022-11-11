86c0d8525f5ca284831a34c0a59901da17b474a4
YangZhou08 <yangzhou25672@utexas.edu>
2022-11-11 12:56:15 -0600

    changeo

--- dlrm_s_pytorch_tb_dp_one.py
+++ dlrm_s_pytorch_tb_dp_one.py
@@ -1883,11 +1883,12 @@ def train(args):
                 mbs = T.shape[0] 
                 
                 X = X[get_my_slice(mbs, world_size, rank)] 
-                print("x {}".format(X.shape)) 
+                print("oooo previously {}".format(lS_o[0][: 100])) 
                 lS_o = lS_o[:, get_my_slice(mbs, world_size, rank)] 
-                print("oooo {}".format(lS_o.shape)) 
+                print("oooo after {}".format(lS_o[0][: 100])) 
+                print("iiii previously {}".format(lS_i[0][: 100])) 
                 lS_i = lS_i[:, get_my_slice(mbs, world_size, rank)] 
-                print("iiii {}".format(lS_i.shape)) 
+                print("iiii after {} ".format(lS_i[0][: 100])) 
                 T = T[get_my_slice(mbs, world_size, rank)] 
                 W = W[get_my_slice(mbs, world_size, rank)] 
                 Z = dlrm_wrap(
