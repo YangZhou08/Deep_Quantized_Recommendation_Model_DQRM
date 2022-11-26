0fa3b1360a1207f5d35b7a1599dc664d3b2972a7
YangZhou08 <yangzhou25672@utexas.edu>
2022-11-25 22:51:27 -0600

    changeo

--- dlrm_s_pytorch_pseudo_cpustb.py
+++ dlrm_s_pytorch_pseudo_cpustb.py
@@ -1592,10 +1592,10 @@ def train(gpu, args):
                 ''' 
                 
                 # used originally 
-                '''
                 grad_buffer_update_added_quantization(dlrm, args.number_of_cpu_node, emb_grad_quantized = True) 
-                ''' 
+                '''
                 grad_buffer_update_added_quantization(dlrm, args.number_of_cpu_node, emb_grad_quantized = False) 
+                ''' 
                 '''
                 optimizer.step() 
                 lr_scheduler.step() 
@@ -1609,11 +1609,11 @@ def train(gpu, args):
                     else: 
                         weights_update_added_quantization(dlrm, lr_scheduler.get_lr()[-1], args.number_of_gpus, emb_grad_quantized = False) 
                     ''' 
-                    '''
                     # used previously 
                     weights_update_added_quantization(dlrm, lr_scheduler.get_lr()[-1], args.number_of_cpu_node, emb_grad_quantized = True, update_embedding = True) 
-                    ''' 
+                    '''
                     weights_update_added_quantization(dlrm, lr_scheduler.get_lr()[-1], args.number_of_cpu_node, emb_grad_quantized = False, update_embedding = True) 
+                    ''' 
                     lr_scheduler.step() 
                     buffer_clean = True 
                 
