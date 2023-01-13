056ef8b1598082d5be4f0aa7bf9f3b927c031135
YangZhou08 <yangzhou25672@utexas.edu>
2023-01-13 09:41:52 -0600

    change script

--- bash_scripts/Terabytes/run_dlrm_one.sh
+++ bash_scripts/Terabytes/run_dlrm_one.sh
@@ -2,4 +2,4 @@ export PATH=$PATH:/opt/pbs/default/bin
 export I_MPI_HYDRA_BOOTSTRAP=rsh
 export I_MPI_HYDRA_BOOTSTRAP_EXEC=pbs_tmrsh
 export LD_LIBRARY_PATH=~/anaconda3/lib 
-bash run_dist.sh -np 2 -ppn 1 -f $PBS_NODEFILE python /homes/yangzhou08/Training_DLRM_fast/python_profiling_script/example_multiple_cpu_dp_two.py 
+bash run_dist.sh -np 2 -ppn 1 -f $PBS_NODEFILE python /homes/yangzhou08/Training_DLRM_fast/dlrm_s_pytorch_tb_dp_one.py 
