c5ed04b29dee7577a557bdd876601a5f60c02371
YangZhou08 <yangzhou25672@utexas.edu>
2023-01-23 11:30:36 -0600

    output check

--- sgd_quantized_gradients_parallel_comm.py
+++ sgd_quantized_gradients_parallel_comm.py
@@ -631,6 +631,10 @@ def weight_update_parallel_comm(model, lr, emb_grad_quantized = True, update_emb
             for layer_one in model.bot_l: 
                 if isinstance(layer_one, QuantLinear) or isinstance(layer_one, LinearCompressedGrad): 
                     if mlp_layer_quantized: 
+                        print(layer_one.weight.grad[0]) 
+                        print(layer_one.weight_scaling_factor) 
+                        print(layer_one.bias.grad) 
+                        print(layer_one.bias_scaling_factor) 
                         layer_one.weight.data.add_(-lr * layer_one.weight.grad * layer_one.weight_scaling_factor.view(-1, 1)) 
                         layer_one.bias.data.add_(-lr * layer_one.bias.grad * layer_one.bias_scaling_factor) 
                     else: 
@@ -643,6 +647,10 @@ def weight_update_parallel_comm(model, lr, emb_grad_quantized = True, update_emb
             for layer_one in model.top_l: 
                 if isinstance(layer_one, QuantLinear) or isinstance(layer_one, LinearCompressedGrad): 
                     if mlp_layer_quantized: 
+                        print(layer_one.weight.grad[0]) 
+                        print(layer_one.weight_scaling_factor) 
+                        print(layer_one.bias.grad) 
+                        print(layer_one.bias_scaling_factor) 
                         layer_one.weight.data.add_(-lr * layer_one.weight.grad * layer_one.weight_scaling_factor.view(-1, 1)) 
                         layer_one.bias.data.add_(-lr * layer_one.bias.grad * layer_one.bias_scaling_factor) 
                     else: 
