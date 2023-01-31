675a38f68714ce46d398bac39f7160dd9c322c82
YangZhou08 <yangzhou25672@utexas.edu>
2023-01-31 12:48:49 -0600

    path difference

--- python_profiling_script/looking_into_tables_and_quantize.py
+++ python_profiling_script/looking_into_tables_and_quantize.py
@@ -108,7 +108,7 @@ for file_name in file_names:
     chicanes = [] 
     ''' 
     
-    plt.savefig("~/" + file_name[: -4] + "hist.png") 
+    plt.savefig(file_name[: -4] + "hist.png") 
     plt.clf() 
     
     print("min: {}, max: {}, mean: {}, standard deviation: {}".format(np.min(list_one), np.max(list_one), np.mean(list_one), np.std(list_one))) 
