db5b5bfed3f93c220412e93c4d0842d982496819
YangZhou08 <yangzhou25672@utexas.edu>
2023-04-24 02:15:49 -0500

    nno

--- dlrm_s_pytorch_hybrid_multi_gpu.py
+++ dlrm_s_pytorch_hybrid_multi_gpu.py
@@ -1192,8 +1192,10 @@ def run():
         args.quantize_activation = False 
     
     args.world_size = args.gpus * args.nodes # world size now calculated by number of gpus and number of nodes 
-    
+    '''
     os.environ['MASTER_ADDR'] = '10.157.244.233' 
+    ''' 
+    os.environ['MASTER_ADDR'] = '169.254.3.1' 
     os.environ['MASTER_PORT'] = '29510' 
     os.environ['WORLD_SIZE'] = str(args.world_size) 
     mp.spawn(train, nprocs = args.gpus, args = (args,)) 
