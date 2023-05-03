9723a73ae4c77886905fa0c6f48174de4796d938
YangZhou08 <yangzhou25672@utexas.edu>
2023-05-03 01:12:59 -0500

    no backward pass

--- extended_distributed_two.py
+++ extended_distributed_two.py
@@ -498,7 +498,7 @@ class All2All_Wait(Function):
                 a2a_info.table_split_lengths,
                 async_op=True,
             )
-            grad_input = grad_input.to(torch.float32) # convert back to float32
+            # grad_input = grad_input.to(torch.float32) # convert back to float32 
             myreq.req = req
             myreq.tensor = grad_input
             return (grad_output,)
