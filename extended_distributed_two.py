5dcd64e67fa06ce71312a5a5b7ab684d98ce6140
YangZhou08 <yangzhou25672@utexas.edu>
2023-05-04 23:31:08 -0500

    tensor

--- extended_distributed_two.py
+++ extended_distributed_two.py
@@ -443,7 +443,7 @@ class All2All_Req(Function):
             a2a_info.table_split_lengths = table_split_lengths
             myreq.a2a_info = a2a_info
             ctx.a2a_info = a2a_info
-            return myreq
+            return myreq.tensor 
 
     @staticmethod
     def backward(ctx, *grad_output):
