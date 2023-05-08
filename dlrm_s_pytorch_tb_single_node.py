70c71efd4a3abafabdaf98696d3d9dbf26577065
YangZhou08 <yangzhou25672@utexas.edu>
2023-05-08 12:10:04 -0500

    nobreak

--- dlrm_s_pytorch_tb_single_node.py
+++ dlrm_s_pytorch_tb_single_node.py
@@ -2069,7 +2069,6 @@ def train(args):
 
                     total_iter = 0
                     total_samp = 0 
-                    break 
                 
                 if should_test: 
                     # test on the first gpu on the first node 
@@ -2138,7 +2137,6 @@ def train(args):
                 dlrm.module.show_output_linear_layer_grad() # checking whether the layer is consistent 
                 ''' 
             k += 1 
-            break 
                             
     else: 
         print("Testing for inference only") 
