327fa25de381c335fa14bb80af57d15f8e6db99c
YangZhou08 <yangzhou25672@utexas.edu>
2023-06-27 22:40:09 +0100

    args

--- dlrm_s_pytorch_hp_with_quantization.py
+++ dlrm_s_pytorch_hp_with_quantization.py
@@ -526,7 +526,7 @@ class DLRM_Net(nn.Module):
             return x 
         ''' 
         count = 0 
-        if self.args.quantization_flag: 
+        if args.quantization_flag: 
             for layer in layers.module: 
                 if isinstance(layer, QuantLinear): 
                     global change_bitw, change_bitw2 
